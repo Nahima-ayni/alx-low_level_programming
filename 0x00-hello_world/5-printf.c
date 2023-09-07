92585170abd8642a5c06f60b6e7f37da45ef62b6
Nahima-ayni <nahimamadey1@gmail.com>
2023-09-07 13:40:18 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include	<stdio.h>
+#include <stdio.h>
 /**
- * main-c programming
- *return: always 0 (success)
- */
-int	main(void)
+ * main - c programming is awesome
+ * Return: Always 0 (success)
+*/
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
