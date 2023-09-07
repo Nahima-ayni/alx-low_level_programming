78d64ba0183d3291aabf3e1e426d99dfaa80e5f9
Nahima-ayni <nahimamadey1@gmail.com>
2023-09-07 13:27:27 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include	<stdio.h>
 /**
- *main - c programming is awesome
+ * main-c programming
  *return: always 0 (success)
  */
 int	main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
