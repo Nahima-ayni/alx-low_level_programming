45293a8710ccd1e93114d80a3f82cd4caa0fb115
Nahima-ayni <nahimamadey1@gmail.com>
2023-09-07 12:04:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-include <	stdio.h	>
+#include <stdio.h>
 /**
  * main - This is c programming language
  * Return: Always 0 (success)
 */
-int	main(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
