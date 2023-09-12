8a4579b91df7d8deddafae540c769b542b04add7
Nahima-ayni <nahimamadey1@gmail.com>
2023-09-12 11:14:04 -0700

    header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
 #include "main.h"
+#include <unistd>
 /**
  * main- c functions
+ * Description for the _putchar
+ * _putchar - prints putchar
  *
- * return: 0 success
+ * return: 0 Success
  *
 */
-
 int main(void)
 {
-	putchar(_putchar);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r'\n);
 
 return (0);
 }
