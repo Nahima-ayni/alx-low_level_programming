90316655dfc3c9cdf5fbd6609dcf5514b7d8abc9
Nahima-ayni <nahimamadey1@gmail.com>
2023-09-14 03:41:36 -0700

    3-print_remaining_days.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,25 +1,26 @@
 #include "main.h"
+
 /**
-* largest_number - returns the largest of 3 numbers
-* @a: first integer
-* @b: second integer
-* @c: third integer
-* Return: largest number
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
 */
+
 int largest_number(int a, int b, int c)
 {
 	int largest = a;
 
-
-	if (b > a)
+	if (b > largest)
 	{
 		largest = b;
 	}
-
-	if (c > a)
+	
+	if (c > largest)
 	{
 		largest = c;
 	}
 
-return (largest);
+	return (largest);
 }
