15b99694f1d150441d20f06e60fa1e138d7916e5
Nahima-ayni <nahimamadey1@gmail.com>
2023-09-14 09:49:30 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply int a and int b
+ * @a: int a
+ * @b: int b multiply a and b
+ *
+ * Return: result for success
+*/
+int mul(int a, int b)
+{
+	int result = (a * b);
+
+	return (result);
+}
