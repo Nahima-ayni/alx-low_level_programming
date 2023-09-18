7fe967306171475aeae989e4adefdf70e72ef850
Nahima-ayni <nahimamadey1@gmail.com>
2023-09-18 10:26:17 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - string
+ * @s: _strlen  integer
+ * Return: (n);
+*/
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; *s != '\0'; n++)
+	s++;
+
+	return (n);
+}
