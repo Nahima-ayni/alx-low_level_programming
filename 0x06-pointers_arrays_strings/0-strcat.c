ed0bac6e20de791027fdc34679911c7a44c38362
Nahima-ayni <nahimamadey1@gmail.com>
2023-09-20 11:06:32 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _strcat - combine two strings
+  * @src: source
+  * @dest: destination
+  * Return: return dest as success
+*/
+char *_strcat(char *dest, char *src)
+{
+	int m, n;
+
+	for (m = 0; dest[m] != '\0'; m++) {
+	}
+
+	for (n = 0; src[n] != '\0'; n++)
+	{
+		dest[m + n] = src[n];
+	}
+
+	dest[m + n] = '\0';
+
+	return dest;
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+on this project we are learning more about the pointers, arrays and strings but we will be doing more of the string. On strings, we will be learning about strcmp, strcat, gets, puts, scanf, strlen
