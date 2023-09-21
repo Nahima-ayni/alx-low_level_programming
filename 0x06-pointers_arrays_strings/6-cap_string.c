05c64e28100a0787bbcb73c9b6f85758e57d5ede
Nahima-ayni <nahimamadey1@gmail.com>
2023-09-21 12:59:01 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,31 +7,31 @@
  */
 char *cap_string(char *ptr)
 {
-        int x = 0;
+	int x = 0;
 
-        while (ptr[x])
-        {
-                while (!(ptr[x] >= 'a' && ptr[x] <= 'z'))
-                        x++;
+	while (ptr[x])
+	{
+		while (!(ptr[x] >= 'a' && ptr[x] <= 'z'))
+		x++;
 
-                if (ptr[x - 1] == ' '  ||
-                    ptr[x - 1] == '\t' ||
-                    ptr[x - 1] == '\n' ||
-                    ptr[x - 1] == '.' ||
-                    ptr[x - 1] == ',' ||
-                    ptr[x - 1] == '!' ||
-                    ptr[x - 1] == '?' ||
-                    ptr[x - 1] == '{' ||
-                    ptr[x - 1] == '}' ||
-                    ptr[x - 1] == ';' ||
-                    ptr[x - 1] == '(' ||
-                    ptr[x - 1] == ')' ||
-                    ptr[x - 1] == '"' ||
-                    x == 0)
-                        ptr[x] -= 32;
+		if (ptr[x - 1] == ' '  ||
+			ptr[x - 1] == '\t' ||
+			ptr[x - 1] == '\n' ||
+        		ptr[x - 1] == '.' ||
+			ptr[x - 1] == ',' ||
+			ptr[x - 1] == '!' ||
+			ptr[x - 1] == '?' ||
+			ptr[x - 1] == '{' ||
+			ptr[x - 1] == '}' ||
+			ptr[x - 1] == ';' ||
+			ptr[x - 1] == '(' ||
+			ptr[x - 1] == ')' ||
+			ptr[x - 1] == '"' ||
+			x == 0)
+		ptr[x] -= 32;
 
-                x++;
-        }
+		x++;
+	}
 
-        return (ptr);
+	return (ptr);
 }
