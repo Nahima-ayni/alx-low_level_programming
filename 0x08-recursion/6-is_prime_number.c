de9b861f2d478350b6ee4355d409564d2b64d7f7
Nahima-ayni <nahimamadey1@gmail.com>
2023-09-27 12:56:33 -0700

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -33,7 +33,8 @@ int is_prime_helper(int n, int divisor)
 int is_prime_number(int n)
 {
 	if (n <= 1)
+	{
 		return (0);
+	}
 	return (is_prime_helper(n, n - 1));
 }
-	
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,6 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int _sqrt_help(int n, int x);
+int is_prime_helper(int n, int divisor);
 
 #endif
