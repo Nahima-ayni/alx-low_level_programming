5191809e18d3b8fb2be2580615cdb3ed678d941e
Nahima-ayni <nahimamadey1@gmail.com>
2023-09-29 09:59:56 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/** whatsmyname - prints a new mine
+ * @argc: agrmunet count
+ * @argv: argument vector
+ */
+int _whatsmyname(char argc, char *argv[])
+{
+	printf("%0s\n", argv[0]);
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+0-whatsmyname.c:  argv and argc to create a mynameis and change it to mynewname is
