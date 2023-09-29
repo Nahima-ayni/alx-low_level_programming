1ccd68d1d93e24d7f04a87bbddd404f98c32c07b
Nahima-ayni <nahimamadey1@gmail.com>
2023-09-29 12:22:42 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,11 +9,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
+	if (argv != NULL && argv[0] != NULL)
 	{
-		printf("%s\n", argv[i]);
+		printf("%d\n", argc - 1);
 	}
 
 	return (0);
