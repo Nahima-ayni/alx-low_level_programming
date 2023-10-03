f35ec04db8f870a5e326f6cc13d1daaeb12cbe12
Nahima-ayni <nahimamadey1@gmail.com>
2023-10-03 10:28:42 -0700

    concatenates all the arguments of your program

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - 
+ * @ac: integer
+ * @av: pointer to  char 
+ * Return: always success
+ */
+char *argstostr(int ac, char **av)
+{
+	char *combination;
+	int length = 0;
+	int i, j;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; i++)
+		length++;
+	}
+	length++;
+	length++;
+	
+	combination = (char *)malloc(length * sizeof(char));
+	if (combination == NULL)
+	{
+		return (NULL);
+	}
+
+	length = 0;
+	for (i = 0; i < ac; i++)
+        {
+                for (j = 0; av[i][j] != '\0'; i++)
+		{
+			combination[length++] = av[i][j];
+		}
+	combination[length++] = '\n';
+	}
+
+	combination[length++] = '\0';
+	return (combination);
+}
