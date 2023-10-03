82662d66937b0e28b8eac3d2e75b1184eb1c55f0
Nahima-ayni <nahimamadey1@gmail.com>
2023-10-03 02:41:37 -0700

    pointer to a 2D

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -33,12 +33,10 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(int));
 		if ((grid[i]) == NULL)
 		{
-			return (NULL);
-		}
-
-		for (j = 0; j < width; j++)
-		{
-			grid[i][j] = 0;
+			for (j = 0; j < width; j++)
+			{
+				free(grid[j]);
+			}
 		}
 	}
 
