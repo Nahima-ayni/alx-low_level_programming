8293e2acf8dcd68e3bb2e5183f7ece236e7e8872
Nahima-ayni <nahimamadey1@gmail.com>
2023-10-09 08:43:00 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,24 +1,6 @@
-#include <stdio.h>
 #ifndef function_like_macro
 #define function_like_macro
 
-int main(void);
+#define ABS(x) ((x) > 0 ? -(x) : (x))
 
-#ifndef ABS
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-#endif
-
-/**
- * main - absolute value
- * Return: always success
- */
-
-int main(void)
-{
-
-	int number = 98;
-
-	printf("%d\n", ABS(number));
-	return (0);
-}
 #endif
