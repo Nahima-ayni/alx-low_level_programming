a0997adcd042b84f9cd6dd96b32097d5d3924930
Nahima-ayni <nahimamadey1@gmail.com>
2023-10-09 13:50:24 -0700

    typedef dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,14 @@
  * @name: char
  * @owner: char
  */
-typedef struct dog dog_t;
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
