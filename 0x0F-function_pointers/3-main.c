55bfc89bc015f630b6e6f2341a8d0bd7625690c9
Nahima-ayni <nahimamadey1@gmail.com>
2023-10-11 12:18:12 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2;
-	char *operator;
+	char *op;
 	int result = 0;
 
 	if (argc != 4)
@@ -21,18 +21,20 @@ int main(int argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-	operator = argv[2];
+	op = argv[2];
 	num2 = atoi(argv[3]);
-
-	if ((get_op_func(operator) == NULL)
-		|| (operator[1] != '\0'))
+	
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	result = get_op_func(operator)(num1, num2);
-	printf("%d\n", result);
-
-	return (0);
+	if ((*op == '/' && num2 == 0) ||
+		(*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+                exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
+	return (0);
 }
