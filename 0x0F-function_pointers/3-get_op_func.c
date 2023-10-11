5b386bc833a2aeab96e652b4d09c08c239105cff
Nahima-ayni <nahimamadey1@gmail.com>
2023-10-11 13:40:27 -0700

    3-functions pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,25 +3,26 @@
 #include <stdlib.h>
 
 /**
- * get_op_func - function pointer
- * @s: char pointer
- * Return: always success
+ * get_op_func - select the option to perform atithmetic op
+ * @s: operator argument
+ * Return: result of selected choice
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
 	};
 	int i;
 
 	i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) == *s)
-		i++;
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	i++;
+
 	return (ops[i].f);
 }
