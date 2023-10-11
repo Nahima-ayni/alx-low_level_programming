f07fb87481ec7ff3f2a81c6ddead732f718ce203
Nahima-ayni <nahimamadey1@gmail.com>
2023-10-11 11:47:07 -0700

    3-functions pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 
 	if ((get_op_func(operator) == NULL)
-                        || (operator[1] != '\0'))
+		|| (operator[1] != '\0'))
 	{
 		printf("Error\n");
 		exit(99);
