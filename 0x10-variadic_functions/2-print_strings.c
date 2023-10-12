e59eeaa39fb478b8cf70d5cdda8b7dc2a1443688
Nahima-ayni <nahimamadey1@gmail.com>
2023-10-12 04:48:40 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -31,7 +31,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 
 
-		if (*separator != '\0' && i < n - 1)
+		if (separator != NULL && *separator != '\0' && i < n - 1)
 		{
 			printf("%s", separator);
 		}
