1397dc3f3f857ab925dcf217dfe6f26c634beeb6
Nahima-ayni <nahimamadey1@gmail.com>
2023-10-24 11:48:13 -0700

    insertion at a given time

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		free(newnode);
 		return (NULL);
 	}
-	newnode = current->next;
+	newnode->next = current->next;
 	current->next = newnode;
 
 	return (newnode);
