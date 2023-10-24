518639495e91674eb4b2000126c84db0e6f0a0de
Nahima-ayni <nahimamadey1@gmail.com>
2023-10-24 11:32:41 -0700

    insertion at a given time

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insertion at a given position
+ * @head: structure double pointer
+ * @idx: unsigned integer
+ * @n: integer
+ * Return: always success
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newnode, *current;
+	size_t i;
+
+	if (*head == NULL)
+		return (NULL);
+
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
+	current = *head;
+	for (i = 0; i < idx - 1 && newnode != NULL; i++)
+	{
+		current = current->next;
+	}
+	if (current == NULL)
+	{
+		free(current);
+		return (current);
+	}
+	newnode = current->next;
+	current->next = newnode;
+
+	return (newnode);
+}
