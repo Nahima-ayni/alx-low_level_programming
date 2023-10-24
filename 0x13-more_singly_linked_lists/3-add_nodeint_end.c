a6afcf9988adfc2588173648facd99546038fc9e
Nahima-ayni <nahimamadey1@gmail.com>
2023-10-23 21:42:47 -0700

    Insertion at the end of the line

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,20 +7,26 @@
  * Return: always success
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
-{       
+{
 	listint_t *last_node, *current;
 
 	last_node = malloc(sizeof(listint_t));
 	if (last_node == NULL)
 		return (NULL);
 
-	new_node->n = data;
-	new_node->next = *head;
+	last_node->n = n;
+	last_node->next = NULL;
 
-	while (current->next != NULL)
+	if (*head == NULL)
+		*head = last_node;
+	else
 	{
-		current = current->next;
-		current->next = last_node;
+		current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+			current->next = last_node;
 	}
 	return (last_node);
 }
