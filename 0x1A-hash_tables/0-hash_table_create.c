1c25f4b7393a52a962fd7e56cfe3ebd3ff77aca7
Nahima-ayni <nahimamadey1@gmail.com>
2023-12-21 22:16:57 +0300

    readme and creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (ht == NULL)
 		return (NULL);
 
-	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t) * size);
 	if (ht->array == NULL)
 	{
@@ -22,6 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
+	ht->size = size;
 	for (i = 0; i < size; i++)
 	{
 		ht->array[i] = NULL;
