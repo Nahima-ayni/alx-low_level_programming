6ccc6aac2b75508259339764562c16bd1b21bba1
Nahima-ayni <nahimamadey1@gmail.com>
2023-12-22 18:44:42 +0300

    function that prints a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,7 +9,6 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned int i;
 	hash_node_t *node;
-	char *c = "";
 
 	if (ht == NULL)
 		return;
@@ -20,8 +19,10 @@ void hash_table_print(const hash_table_t *ht)
 		node = ht->array[i];
 		while (node != NULL)
 		{
-			printf("%s '%s': '%s'", c, node->key, node->value);
-			c = ", ";
+			printf("'%s': '%s' ", node->key, node->value);
+			if (node->next != NULL)
+				printf(", ");
+
 			node = node->next;
 		}
 	}
