a73100fa7bb042db889c2ab17facdf8b83e3832b
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-08 13:13:59 +0300

    binary search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,7 +15,7 @@ int linear_search(int *array, size_t size, int value)
 	int found = 0;
 	size_t i;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
